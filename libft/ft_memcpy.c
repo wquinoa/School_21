41b2a6843ca96aa6a2a14f8488cf0eca9e3204a5
Shimat <philip.yevseev@gmail.com>
2020-05-31 21:11:54 +0300

    reworked memcpy to write more per cycle

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,22 +6,38 @@
 /*   By: wquinoa <wquinoa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 20:18:08 by wquinoa           #+#    #+#             */
-/*   Updated: 2020/05/13 11:45:36 by wquinoa          ###   ########.fr       */
+/*   Updated: 2020/05/31 21:07:34 by wquinoa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+**	Reworked memcpy to copy in blocks of up to 8 bytes per cycle
+**	The memory has to be aligned into a suitable block
+**	byte by byte first. uint_8t is unsigned char.
+*/
+
 void	*ft_memcpy(void *dst, void const *src, size_t n)
 {
-	unsigned char		*a;
-	unsigned char const	*b;
+	const uint8_t	size = sizeof(unsigned long);
+	const uint8_t	*c2 = (uint8_t *)(src);
+	uint8_t			*c1;
+	unsigned long	*a1;
+	unsigned long	*a2;
 
-	if ((!dst && !src) || !n)
-		return (dst);
-	a = (unsigned char *)dst;
-	b = (unsigned char *)src;
-	while (n-- > 0)
-		*(a++) = *(b++);
+	c1 = (uint8_t *)dst;
+	while (n % size)
+	{
+		*(c1++) = *(c2++);
+		n--;
+	}
+	a1 = (unsigned long *)(c1);
+	a2 = (unsigned long *)(c2);
+	while (n >= size)
+	{
+		*(a1++) = *(a2++);
+		n -= size;
+	}
 	return (dst);
 }
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: wquinoa <wquinoa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 18:34:42 by wquinoa           #+#    #+#             */
-/*   Updated: 2020/05/03 20:56:57 by wquinoa          ###   ########.fr       */
+/*   Updated: 2020/05/31 21:09:16 by wquinoa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	b = (unsigned char *)src;
 	i = 0;
 	if (dst < src)
-		while (i < len)
-		{
-			a[i] = b[i];
-			i++;
-		}
+		ft_memcpy(dst, src, len);
 	else if (dst > src)
 		while (++i <= len)
 			a[len - i] = b[len - i];
